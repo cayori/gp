67632b0b5ab61654405eaf645c4e149dadb9e5c1
JPBC\JPBC <cayori@gmail.com>
2018-05-23 11:46:25 +0900

    no message

---
+++ repos/gp/CheckBoxProgram/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CheckBoxProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChecked = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCheckState = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Checked";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_Click);
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_MouseEnter);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(21, 45);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(89, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "UnChecked";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_Click);
+            this.checkBox2.CheckStateChanged += new System.EventHandler(this.checkBox2_MouseEnter);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(22, 67);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(100, 16);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Indeterminate";
+            this.checkBox3.ThreeState = true;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_Click);
+            this.checkBox3.CheckStateChanged += new System.EventHandler(this.checkBox3_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Checked 속성";
+            // 
+            // txtChecked
+            // 
+            this.txtChecked.Location = new System.Drawing.Point(135, 39);
+            this.txtChecked.Name = "txtChecked";
+            this.txtChecked.Size = new System.Drawing.Size(125, 21);
+            this.txtChecked.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CheckState 속성";
+            // 
+            // txtCheckState
+            // 
+            this.txtCheckState.Location = new System.Drawing.Point(135, 99);
+            this.txtCheckState.Name = "txtCheckState";
+            this.txtCheckState.Size = new System.Drawing.Size(125, 21);
+            this.txtCheckState.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtCheckState);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtChecked);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChecked;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCheckState;
+    }
+}
+
---
+++ repos/gp/CheckBoxProgram/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckBoxProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckThis(object sender, EventArgs e)
+        {
+            CheckBox chBox = (CheckBox)sender;
+            txtChecked.Text = chBox.Checked ? "True" : "False";
+
+            switch (chBox.CheckState)
+            {
+                case CheckState.Checked:
+                    txtCheckState.Text = "Checked";
+                    break;
+                case CheckState.Unchecked:
+                    txtCheckState.Text = "UnChecked";
+                    break;
+                case CheckState.Indeterminate:
+                    txtCheckState.Text = "Indeterminate";
+                    break;
+            }
+        }
+
+        private void checkBox1_MouseEnter(object sender, EventArgs e)
+        {
+            CheckThis(sender, e);
+        }
+
+        private void checkBox2_MouseEnter(object sender, EventArgs e)
+        {
+            CheckThis(sender, e);
+        }
+        private void checkBox3_MouseEnter(object sender, EventArgs e)
+        {
+            CheckThis(sender, e);
+        }
+        private void checkBox3_Click(object sender, EventArgs e)
+        {
+            CheckThis(sender, e);
+        }
+        private void checkBox2_Click(object sender, EventArgs e)
+        {
+            CheckThis(sender, e);
+        }
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            CheckThis(sender, e);
+        }
+    }
+}
